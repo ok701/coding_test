a52d9bc2b7a07fba7e512f3d918113555d2b6ea9
ok701 <77150437+ok701@users.noreply.github.com>
2023-04-02 22:55:00 +0900

    Update ex.py

--- 프로그래머스/lv2/42747. H－Index/ex.py
+++ 프로그래머스/lv2/42747. H－Index/ex.py
@@ -1,4 +1,12 @@
-def solution(citations):
+def solution1(citations):
     citations.sort(reverse=True)
     answer = max(map(min, enumerate(citations, start=1)))
     return answer
+
+def solution2(citations):
+    citations = sorted(citations)
+    l = len(citations)
+    for i in range(l):
+        if citations[i] >= l-i:
+            return l-i
+    return 0
