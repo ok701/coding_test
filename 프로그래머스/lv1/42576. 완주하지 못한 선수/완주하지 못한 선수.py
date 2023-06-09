07ef31769ea4f2f63302165ab083425a46af9ae7
ok701 <77150437+ok701@users.noreply.github.com>
2023-06-09 14:30:31 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 78.33 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,18 +1,24 @@
-def solution(participant, completion):
+# def solution(participant, completion):
     
-    dic = {}
-    for a in participant:
-        dic[a] = 0
+#     dic = {}
+#     for a in participant:
+#         dic[a] = 0
 
-    for a in participant:
-        dic[a] += 1
+#     for a in participant:
+#         dic[a] += 1
 
-    for a in completion:
-        dic[a] -= 1
+    
+#     for a in completion:
+#         dic[a] -= 1
         
-    for i in dic:
-        if dic[i] != 0:
-             answer = i
+#     for i in dic:
+#         if dic[i] != 0:
+#              answer = i
              
   
-    return answer
+#     return answer
+
+from collections import Counter
+
+def solution(participant, completion):
+    return ''.join(Counter(participant) - Counter(completion))
